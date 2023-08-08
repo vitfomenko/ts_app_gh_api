d65920cf76cb90e367d7731171d29015bb9ad1a2
Vitalii Fomenko <vfomenko@Vitaliis-MacBook-Pro-23825.local>
2023-08-08 16:07:23 +0300

    added pages and navigations

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import FavouritesPage from './pages/FavouritesPage';
+import HomePage from './pages/HomePage';
+
 function App() {
-	return <h1 className="font-bold">Hello React!</h1>;
+	return (
+		<>
+			<Navigation />
+			<Routes>
+				<Route
+					path="/"
+					element={<HomePage />}
+				/>
+				<Route
+					path="/favourites"
+					element={<FavouritesPage />}
+				/>
+			</Routes>
+		</>
+	);
 }
 
 export default App;
